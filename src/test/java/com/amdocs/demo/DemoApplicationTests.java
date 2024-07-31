df1dac44da29d016f523440bf0b63fceac3c5fda
Pavan Kumar <91956183+PavanKumarPendyala@users.noreply.github.com>
2024-07-31 22:40:33 +0530

    DemoApplicationTests.java

--- src/test/java/com/amdocs/demo/DemoApplicationTests.java
+++ src/test/java/com/amdocs/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.amdocs.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+@Controller
+public class DemoApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("mainMessage", "Welcome to My Awesome World!");
+        model.addAttribute("subMessage", "Explore the wonders of web development");
+        return "home";
+    }
 }
